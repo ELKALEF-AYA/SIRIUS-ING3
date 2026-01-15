12446b4153b3036ee3e9c7ee77e3e7dfd0cf4f83
Aya El Kalef <aya40146@gmail.com>
2026-01-15 22:04:29 +0100

    update message class

--- chat/src/main/java/com/app/chat/model/Message.java
+++ chat/src/main/java/com/app/chat/model/Message.java
@@ -1,9 +1,6 @@
 package com.app.chat.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
@@ -15,17 +12,76 @@ public class Message {
 
     private Long senderId;
     private Long receiverId;
+    private String senderType;
+
     private String content;
+
+    private Long conversationId;
+
     private LocalDateTime timestamp = LocalDateTime.now();
 
     public Message() {}
 
-    public Message(Long senderId, Long receiverId, String content) {
+    public Message(Long senderId, Long receiverId, String senderType, String content, Long conversationId) {
         this.senderId = senderId;
         this.receiverId = receiverId;
+        this.senderType = senderType;
         this.content = content;
+        this.conversationId = conversationId;
         this.timestamp = LocalDateTime.now();
     }
 
-    // getters et setters
+    // GETTERS + SETTERS
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(Long senderId) {
+        this.senderId = senderId;
+    }
+
+    public Long getReceiverId() {
+        return receiverId;
+    }
+
+    public void setReceiverId(Long receiverId) {
+        this.receiverId = receiverId;
+    }
+
+    public String getSenderType() {
+        return senderType;
+    }
+
+    public void setSenderType(String senderType) {
+        this.senderType = senderType;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Long getConversationId() {
+        return conversationId;
+    }
+
+    public void setConversationId(Long conversationId) {
+        this.conversationId = conversationId;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
 }
