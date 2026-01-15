cb5cbf985bb0098c979553be43420754f90b9f1a
Aya El Kalef <aya40146@gmail.com>
2026-01-15 21:50:10 +0100

    Add ChatService (save and get conversation)

---
+++ chat/src/main/java/com/app/chat/service/ChatService.java
@@ -0,0 +1,27 @@
+package com.app.chat.service;
+
+import com.app.chat.model.Message;
+import com.app.chat.repository.ChatRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ChatService {
+
+    private final ChatRepository chatRepository;
+
+    public ChatService(ChatRepository chatRepository) {
+        this.chatRepository = chatRepository;
+    }
+
+
+    public Message saveMessage(Message message) {
+        return chatRepository.save(message);
+    }
+
+
+    public List<Message> getConversation(Long senderId, Long receiverId) {
+        return chatRepository.findBySenderIdAndReceiverId(senderId, receiverId);
+    }
+}
