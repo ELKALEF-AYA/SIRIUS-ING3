a5098aa13e0d79a27ab7abd95cd5545baa442957
Atiq-Jihane <atiqjihane123@gmail.com>
2026-01-16 09:48:24 +0100

    Adding pom.xml

---
+++ jsaHome/src/main/java/com/jsahome/RentReceipt.java
@@ -0,0 +1,12 @@
+package src.main.java.com.jsahome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentReceipt {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentReceipt.class, args);
+    }
+}
